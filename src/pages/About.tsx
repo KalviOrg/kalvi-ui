fbb805753f3c92446c827920a9f44944a70402b4
kaushilmrl <kaushik.mrl@gmail.com>
2022-08-22 20:22:15 +0530

    about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,13 +5,24 @@ import Grid from '@mui/material/Grid'
 import Divider from '@mui/material/Divider'
 import { styled } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
-import Box from '@mui/material/Box'
+import Box, { BoxProps } from '@mui/material/Box'
 import CardContent from '@mui/material/CardContent'
 import dynamic from "next/dynamic";
+import Button from "@mui/material/Button";
+import Wallet from 'mdi-material-ui/Wallet'
+
+
 
 const Login = dynamic(() => import("./TestLogin"), {
     ssr: false,
   });
+
+  const StyledBox = styled(Box)<BoxProps>(({ theme }) => ({
+    [theme.breakpoints.up('sm')]: {
+      borderRight: ``
+    }
+  }))
+
 
    // ** logo
    const Img = styled('img')(({ }) => ({
@@ -39,11 +50,21 @@ const About = () => {
             <Typography variant='body2'>
             We created a dApp that can connect Sponsor and Learner together to achieve learning goals, that can create business impact for the organization. Sponsor can signup, onboard learners, create courses, assign reward for courses. Learner can learn the courses, take assessment, receive proof of milestone NFTS and earn rewards in USDCx super token. Learner can unwrap the token to stable coin and withdraw to their exchange account.
             </Typography>
-            <Divider sx={{ marginTop: 6.5, marginBottom: 6.75 }} />
-              <Grid item xs={12} sm={7}>
-                <Login/>
+          
+              <Divider sx={{ marginTop: 6.5, marginBottom: 6.75 }} />
+            <Grid container spacing={4}>
+              <Grid item xs={12} sm={5}>
+                <StyledBox>
+                  <Box sx={{ mb: 6.75, display: 'flex', alignItems: 'center' }}>
+                    <Login/>
+                  </Box>
+                </StyledBox>
               </Grid>
-            
+              <Grid item xs={12} sm={7}>
+              <Button variant="contained" startIcon={<Wallet/>}>Connect Sequence</Button>
+                
+              </Grid>
+            </Grid>
           </CardContent>
         </Grid>
         <Grid
